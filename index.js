8148aa6c28b36371673fe50bad5f360adfb68cb4
murash4 <singleant@gmail.com>
2021-01-29 00:46:30 +0300

    create index.html for route '/'

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import path from 'path'
+
+const __dirname = path.resolve()
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.get('/', (req, res) => {
+	// res.send('<h1>Hello express</h1>')
+	res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
+})
+
+app.listen(PORT, () => {
+	console.log(`Server has been started on port ${PORT}...`)
+})
